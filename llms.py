2d609a432a49ce945ac2cf7d47cbe2b73be663aa
yexia553 <1906390603@qq.com>
2024-08-19 16:11:21 +0800

    feat: add llm libs

---
+++ llms.py
@@ -0,0 +1,51 @@
+import os
+
+
+azure_gpt4o = {
+    "model": "gpt-4o",
+    "api_key": os.environ.get("OPENAI_API_KEY"),
+    "base_url": os.environ.get("OPENAI_API_BASE"),
+    "api_type": "azure",
+    "api_version": "2023-03-15-preview",
+    "temperature": 0.9,
+}
+
+azure_gpt4o_mini = {
+    "model": "gpt-4o-mini",
+    "api_key": os.environ.get("OPENAI_API_KEY"),
+    "base_url": os.environ.get("OPENAI_API_BASE"),
+    "api_type": "azure",
+    "api_version": "2024-02-15-preview",
+    "temperature": 0.8
+}
+
+kimi_8k = {
+    "model": "moonshot-v1-8k",
+    "api_key": os.environ.get("KIMI_API_KEY"),
+    "base_url": os.environ.get("KIMI_API_BASE"),
+    "temperature": 0.8
+}
+
+# this claude used proexy: https://uniapi.ai/docs/
+claude-3.5-sonnet = {
+    "model": "claude-3-5-sonnet-20240620",
+    "api_key": os.environ.get("UNIAPI_API_KEY"),
+    "base_url": os.environ.get("UNIAPI_API_BASE"),
+    "temperature": 0.8
+}
+
+kimi_32k = {
+    "model": "moonshot-v1-32k",
+    "api_key": os.environ.get("KIMI_API_KEY"),
+    "base_url": os.environ.get("KIMI_API_BASE"),
+    "temperature": 0.8
+}
+
+kimi_128k = {
+    "model": "moonshot-v1-128k",
+    "api_key": os.environ.get("KIMI_API_KEY"),
+    "base_url": os.environ.get("KIMI_API_BASE"),
+    "temperature": 0.8
+}
+
+
